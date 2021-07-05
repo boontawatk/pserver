50d58cc665afce1b4031408908e407c2113094ce
Boontawat Kumpiroj <boontawatk@Boontawats-MacBook-Air.local>
2021-07-05 15:47:33 +0700

    add path

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
     res.send("hello worldddd nodee")
 })
 
+app.get('/hi',(req,res)=>{
+    res.send("hi world node")
+})
+
 app.listen(3000,()=>{
     console.log("start node server at port 3000")
 })
